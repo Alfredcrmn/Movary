dc6c4efa08374550c17c91cef7b1d5e3d0b43563
Luis Alfredo Carmona Martínez <acarmonacuarto@gmail.com>
2025-07-09 22:44:32 -0600

    Created the movie card component. Wrote a temporary test on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import MovieCard from "./components/MovieCard"
 
 function App() {
   return (
-    <div></div>
+    <>
+      <MovieCard movie = {{
+        title: "My movie",
+        release_date: "1990",
+      }}/>
+    </>
+    
   )
 }
-
 export default App
